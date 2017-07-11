02cca8e701687c23117fa5eed9b0dc7a37801aa3
Andrei Goncharov <frantic1993@gmail.com>
2017-07-11 17:45:29 +0300

    Feature Added: Pool transactions (#1)
    * Feature Added: Transactions and initial setup added
    
    * Feature Added: Jenkins' pipeline name changed
    
    Signed-off-by: Andrei Goncharov <frantic1993@gmail.com>

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import sys
+import os
+
+from setuptools import setup, find_packages, __version__
+
+try:
+    SETUP_DIRNAME = os.path.dirname(__file__)
+except NameError:
+    # We're probably being frozen, and __file__ triggered this NameError
+    # Work around this
+    SETUP_DIRNAME = os.path.dirname(sys.argv[0])
+
+if SETUP_DIRNAME != '':
+    os.chdir(SETUP_DIRNAME)
+
+SETUP_DIRNAME = os.path.abspath(SETUP_DIRNAME)
+
+METADATA = os.path.join(SETUP_DIRNAME, 'sovrin', '__metadata__.py')
+# Load the metadata using exec() so we don't trigger an import of ioflo.__init__
+exec(compile(open(METADATA).read(), METADATA, 'exec'))
+
+setup(
+    name='sovrin-dev',
+    version=__version__,
+    description='Sovrin node',
+    url='https://github.com/hyperledger/indy-node',
+    author=__author__,
+    author_email='dev@evernym.us',
+    license=__license__,
+    keywords='Sovrin Genesis Transactions',
+    packages=find_packages(exclude=['docs', 'docs*']) + [
+        'data'],
+    package_data={
+        '': ['*.txt', '*.md', '*.rst', '*.json', '*.conf', '*.html',
+             '*.css', '*.ico', '*.png', 'LICENSE', 'LEGAL', '*.sovrin']},
+    include_package_data=True,
+)
