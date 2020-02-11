cff1f201eb6a69c83e6248a173b234e8121320c3
Michael Lodder <redmike7@gmail.com>
2020-02-11 09:56:40 -0700

    Update setup to be latest from prior stable
    Signed-off-by: Michael Lodder <redmike7@gmail.com>

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ METADATA = os.path.join(SETUP_DIRNAME, 'sovrin', '__metadata__.py')
 exec(compile(open(METADATA).read(), METADATA, 'exec'))
 
 setup(
-    name='sovrin-dev',
+    name='sovrin',
     version=__version__,
     description='Sovrin node',
     url='https://github.com/sovrin-foundation/sovrin',
@@ -37,5 +37,6 @@ setup(
         '': ['*.txt', '*.md', '*.rst', '*.json', '*.conf', '*.html',
              '*.css', '*.ico', '*.png', 'LICENSE', 'LEGAL', '*.indy']},
     include_package_data=True,
-    install_requires=['indy-node', 'sovtoken', 'sovtokenfees'],
+
+    install_requires=['indy-node==1.12.2', 'sovtoken==1.0.7', 'sovtokenfees==1.0.7'],
 )
