5811c7ae0c010fc0c97b16e34951641350ad2e1b
Wade Barnes <wade.barnes@shaw.ca>
2020-08-13 07:40:55 -0700

    [RC-1.1.82] bump indy-node and plugin versions to latest RC
    Signed-off-by: Wade Barnes <wade.barnes@shaw.ca>

--- setup.py
+++ setup.py
@@ -38,5 +38,5 @@ setup(
              '*.css', '*.ico', '*.png', 'LICENSE', 'LEGAL', '*.indy']},
     include_package_data=True,
 
-    install_requires=['indy-node==1.12.3', 'sovtoken==1.0.8', 'sovtokenfees==1.0.8'],
+    install_requires=['indy-node==1.12.4.rc1', 'sovtoken==1.0.9.rc36', 'sovtokenfees==1.0.9.rc36'],
 )
