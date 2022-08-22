2e01ff049b020b73ddaa07c332d49091c55c35d0
Wade Barnes <wade@neoterictech.ca>
2022-08-22 07:37:40 -0700

    [Stable] Change deps to stable versions
    Signed-off-by: Wade Barnes <wade@neoterictech.ca>

--- setup.py
+++ setup.py
@@ -38,5 +38,5 @@ setup(
              '*.css', '*.ico', '*.png', 'LICENSE', 'LEGAL', '*.indy']},
     include_package_data=True,
 
-    install_requires=['indy-node==1.12.6.rc2', 'sovtoken==1.0.11.rc41', 'sovtokenfees==1.0.11.rc41'],
+    install_requires=['indy-node==1.12.6', 'sovtoken==1.0.11', 'sovtokenfees==1.0.11'],
 )
