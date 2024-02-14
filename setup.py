8bdd39c1b8e5e9a7e20e9f824f14d67205504373
Wade Barnes <wade@neoterictech.ca>
2024-02-14 13:12:34 -0800

    Update token-plugin versions
    Signed-off-by: Wade Barnes <wade@neoterictech.ca>

--- setup.py
+++ setup.py
@@ -38,5 +38,5 @@ setup(
              '*.css', '*.ico', '*.png', 'LICENSE', 'LEGAL', '*.indy']},
     include_package_data=True,
 
-    install_requires=['indy-node==1.13.2', 'sovtoken==1.1.0', 'sovtokenfees==1.1.0'],
+    install_requires=['indy-node==1.13.2', 'sovtoken==1.1.1', 'sovtokenfees==1.1.1'],
 )
